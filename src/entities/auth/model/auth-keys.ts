d524507f8eab9d3140eca7b465b8546914d33ad0
ChanLee_KR <cl700120@gmail.com>
2024-05-03 21:28:47 +0900

    fix : 빌드 에러 수정

--- src/entities/auth/model/auth-keys.ts
+++ src/entities/auth/model/auth-keys.ts
@@ -5,6 +5,7 @@ const authKeys = {
   accessToken: () => [...authKeys.all, "accessToken"] as const,
   login: (info: LoginInfo) => [...authKeys.all, "login", info] as const,
   join: (details: ExtraDetails) => [...authKeys.all, "join", details] as const,
+  reissue: () => [...authKeys.all, "reissue"],
   userNameDuplicate: (name: string) =>
     [...authKeys.all, "username-duplicate", { name }] as const,
 };
--- src/entities/auth/utils/with-auth.tsx
+++ src/entities/auth/utils/with-auth.tsx
@@ -3,10 +3,9 @@
 import React from "react";
 import useAccessToken from "../model/use-access-token";
 
-export default function withAuth<P extends React.JSX.IntrinsicAttributes>(
-  Component: React.FC<P>,
-) {
-  return function ProtectedComponent(props: P) {
+export default function withAuth(Component: React.FunctionComponent) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return function ProtectedComponent(props: any) {
     const { accessToken } = useAccessToken();
     console.log(accessToken);
 
@@ -14,7 +13,6 @@ export default function withAuth<P extends React.JSX.IntrinsicAttributes>(
       return null;
     }
 
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    return <Component {...props} />;
+    return React.createElement(Component, props, null);
   };
 }
